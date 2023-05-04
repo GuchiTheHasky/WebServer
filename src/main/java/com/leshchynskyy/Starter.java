629c4233bde9100b164e7be9a068755fe6e34b18
GuchiTheHasky <guchithehasky@gmail.com>
2023-05-04 07:10:39 +0300

    1. Change artifact-id

--- src/main/java/com/leshchynskyy/Starter.java
+++ src/main/java/com/leshchynskyy/Starter.java
@@ -14,8 +14,7 @@ public class Starter {
         }
     }
 
-    public static void main( String[] args )
-    {
+    public static void main(String[] args) {
         int port = 1024;
         String sourcePath = "src\\test\\resources\\webApp";
         Server server = new Server(port, sourcePath);
